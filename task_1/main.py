96382b7a5ff60125aa42b7f098e8fe53aa645412
shebel <Shebelnitskiy@gmail.com>
2024-02-29 13:21:41 +0500

    upgrade task_1

--- task_1/main.py
+++ task_1/main.py
@@ -1,13 +1,12 @@
 def func_sequence(n):
-    sequence = ""
     for i in range(1, n+1):
-        sequence += str(i) * i
-    return sequence
+        for _ in range(i):
+            yield i
 
 def main():
-    n = int(input("Введите количество элементов (n): "))
+    n = int(input("Введите количество элементов: "))
     sequence = func_sequence(n)
-    print(f"Первые {n} элементов последовательности: {sequence[0:n]}")
+    print(f"Первые {n} элементов последовательности: {''.join(str(next(sequence)) for _ in range(n))}")
 
 if __name__ == "__main__":
     main()
